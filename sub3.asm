e2b664c422cb2ed701a6dbfa2cfd5b2e6e613dad
Tony Tang <tangchang21@gmail.com>
2016-06-13 16:50:51 +0800

    fix sub3

--- sub3.asm
+++ sub3.asm
@@ -1,123 +1,115 @@
+;;
+;; file: sub3.asm
+;; Subprogram example program
+;;
+;; To create executable:
+;; Using djgpp:
+;; nasm -f coff sub3.asm
+;; gcc -o sub1 sub3.o driver.c asm_io.o
+;;
+;; Using Borland C/C++
+;; nasm -f obj sub3.asm
+;; bcc32 sub3.obj driver.c asm_io.obj
 
-
-;
-; file: sub3.asm
-; Subprogram example program
-;
-; To create executable:
-; Using djgpp:
-; nasm -f coff sub3.asm
-; gcc -o sub1 sub3.o driver.c asm_io.o
-;
-; Using Borland C/C++
-; nasm -f obj sub3.asm
-; bcc32 sub3.obj driver.c asm_io.obj
+default rel
 
 %include "asm_io.inc"
 
 segment .data
-sum     dd   0
+  sum     dd   0
 
 segment .bss
-input   resd 1
+  input   resd 1
 
- 
-
-;
-; psuedo-code algorithm
-; i = 1;
-; sum = 0;
-; while( get_int(i, &input), input != 0 ) {
-;   sum += input;
-;   i++;
-; }
-; print_sum(num);
+;;
+;; psuedo-code algorithm
+;; i = 1;
+;; sum = 0;
+;; while( get_int(i, &input), input != 0 ) {
+;;   sum += input;
+;;   i++;
+;; }
+;; print_sum(num);
 
 segment .text
-        global  asm_main
-asm_main:
-        enter   0,0               ; setup routine
-        pusha
+global  _asm_main
+_asm_main:
+  enter   0,0               ; setup routine
 
-        mov     edx, 1            ; edx is 'i' in pseudo-code
+  mov     edx, 1            ; edx is 'i' in pseudo-code
 while_loop:
-        push    edx               ; save i on stack
-        push    dword input       ; push address on input on stack
-        call    get_int
-        add     esp, 8            ; remove i and &input from stack
+  mov     rax, input
+  push    rax               ; push address on input on stack
+  push    rdx               ; edx -> rdx
+  call    get_int
+  mov     rdx, [rsp]
+  add     rsp, 0x10             ; remove i and &input from stack
 
-        mov     eax, [input]
-        cmp     eax, 0
-        je      end_while
+  mov     eax, [input]
+  cmp     eax, 0
+  je      end_while
 
-        add     [sum], eax        ; sum += input
+  add     [sum], eax        ; sum += input
 
-        inc     edx
-        jmp     short while_loop
+  inc     edx
+  jmp     while_loop
 
 end_while:
-        push    dword [sum]       ; push value of sum onto stack
-        call    print_sum
-        pop     ecx               ; remove [sum] from stack
+  sub     rsp, 0x08         ; align to 0x10, 0x08 = 0x10 - 0x08
+  mov     eax, dword [sum]
+  push    rax                   ; push value of sum onto stack, eax -> rax
+  call    print_sum
+  add     rsp, 0x10
 
-        popa
-        leave                     
-        ret
+  leave
+  ret
 
-;
-; subprogram get_int
-; Parameters (in order pushed on stack)
-;   number of input (at [ebp + 12])
-;   address of word to store input into (at [ebp + 8])
-; Notes:
-;   values of eax and ebx are destroyed
+;;
+;; subprogram get_int
+;; Parameters (in order pushed on stack)
+;;   number of input (at [rbp + 0x10])
+;;   address of word to store input into (at [rbp + 0x18])
+;; Notes:
+;;   values of eax and ebx are destroyed
 segment .data
-prompt  db      ") Enter an integer number (0 to quit): ", 0
+  prompt  db      ") Enter an integer number (0 to quit): ", 0
 
 segment .text
 get_int:
-        push    ebp
-        mov     ebp, esp
+  enter   0, 0
 
-        mov     eax, [ebp + 12]
-        call    print_int
+  mov     rax, [rbp + 0x10]
+  call    print_int
 
-        mov     eax, prompt
-        call    print_string
-        
-        call    read_int
-        mov     ebx, [ebp + 8]
-        mov     [ebx], eax         ; store input into memory
+  mov     rax, prompt
+  call    print_string
 
-        pop     ebp
-        ret                        ; jump back to caller
+  call    read_int
+  mov     rbx, [rbp + 0x18]
+  mov     [rbx], eax         ; store input into memory
 
-; subprogram print_sum
-; prints out the sum
-; Parameter:
-;   sum to print out (at [ebp+8])
-; Note: destroys value of eax
-;
+  leave
+  ret                        ; jump back to caller
+
+;; subprogram print_sum
+;; prints out the sum
+;; Parameter:
+;;   sum to print out (at [rbp+0x10])
+;; Note: destroys value of eax
+;;
 segment .data
-result  db      "The sum is ", 0
+  result  db      "The sum is ", 0
 
 segment .text
 print_sum:
-        push    ebp
-        mov     ebp, esp
+  enter   0, 0
 
-        mov     eax, result
-        call    print_string
+  mov     rax, result
+  call    print_string
 
-        mov     eax, [ebp+8]
-        call    print_int
-        call    print_nl
+  mov     rax, [rbp + 0x10]     ; rax -> eax
+  call    print_int
+  call    print_nl
 
-        pop     ebp
-        ret
-
-
-
-
-
-
+  leave
+  ret
