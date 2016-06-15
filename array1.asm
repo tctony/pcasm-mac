bd8dd943696a9844d4d08c2a502ec8728b871458
Tony Tang <tangchang21@gmail.com>
2016-06-15 19:46:14 +0800

    fix array1

--- array1.asm
+++ array1.asm
@@ -1,135 +1,118 @@
-
-
-;
-; file: array1.asm
-; This program demonstrates arrays in assembly
-;
-; To create executable:
-; nasm -f coff array1.asm
-; gcc -o array1 array1.o array1c.c
-;
+;;
+;; file: array1.asm
+;; This program demonstrates arrays in assembly
+;;
+;; To create executable:
+;; nasm -f coff array1.asm
+;; gcc -o array1 array1.o array1c.c
+;;
 
 %define ARRAY_SIZE 100
 %define NEW_LINE 10
 
 segment .data
-FirstMsg        db   "First 10 elements of array", 0
-Prompt          db   "Enter index of element to display: ", 0
-SecondMsg       db   "Element %d is %d", NEW_LINE, 0
-ThirdMsg        db   "Elements 20 through 29 of array", 0
-InputFormat     db   "%d", 0
+  FirstMsg        db   "First 10 elements of array", 0
+  Prompt          db   "Enter index of element to display: ", 0
+  SecondMsg       db   "Element %d is %d", NEW_LINE, 0
+  ThirdMsg        db   "Elements 20 through 29 of array", 0
+  InputFormat     db   "%d", 0
 
 segment .bss
-array           resd ARRAY_SIZE
-
- 
+  array           resd ARRAY_SIZE
 
 segment .text
-        extern  puts, printf, scanf, dump_line
-        global  asm_main
-asm_main:
-        enter   4,0             ; local dword variable at EBP - 4
-        push    ebx
-        push    esi
+extern  _puts, _printf, _scanf, _dump_line
+global  _asm_main
+_asm_main:
+  enter   0x10, 0             ; local dword variable at EBP - 4
 
-; initialize array to 100, 99, 98, 97, ...
+;; initialize array to 100, 99, 98, 97, ...
 
-        mov     ecx, ARRAY_SIZE
-        mov     ebx, array
+  mov     ecx, ARRAY_SIZE
+  mov     rbx, array
 init_loop:
-        mov     [ebx], ecx
-        add     ebx, 4
-        loop    init_loop
+  mov     [rbx], ecx
+  add     rbx, 4
+  loop    init_loop
+  mov     rbx, array
 
-        push    dword FirstMsg       ; print out elements 20-29
-        call    puts           ; print out FirstMsg
-        pop     ecx
+  mov     rdi, FirstMsg
+  call    _puts
 
-        push    dword 10
-        push    dword array
-        call    print_array           ; print first 10 elements of array
-        add     esp, 8
+  mov     rdi, array
+  mov     rsi, 10
+  call    print_array           ; print first 10 elements of array
 
-; prompt user for element index
+;; prompt user for element index
 Prompt_loop:
-        push    dword Prompt
-        call    printf
-        pop     ecx
+  sub     rsp, 0x10
+  mov     rdi, Prompt
+  call    _printf
 
-        lea     eax, [ebp-4]      ; eax = address of local dword
-        push    eax
-        push    dword InputFormat
-        call    scanf
-        add     esp, 8
-        cmp     eax, 1               ; eax = return value of scanf
-        je      InputOK
+  mov     rdi, InputFormat
+  mov     rsi, rsp
+  call    _scanf
+  cmp     rax, 1               ; eax = return value of scanf
+  je      InputOK
 
-        call    dump_line  ; dump rest of line and start over
-        jmp     Prompt_loop          ; if input invalid
+  call    _dump_line  ; dump rest of line and start over
+  add     rsp, 0x10
+  jmp     Prompt_loop          ; if input invalid
 
 InputOK:
-        mov     esi, [ebp-4]
-        push    dword [array + 4*esi]
-        push    esi
-        push    dword SecondMsg      ; print out value of element
-        call    printf
-        add     esp, 12
-        push    dword ThirdMsg       ; print out elements 20-29
-        call    puts
-        pop     ecx
+  mov     rdi, SecondMsg      ; print out value of element
+  mov     esi, [rsp]
+  mov     rdx, [rbx+4*rsi]
+  call    _printf
 
-        push    dword 10
-        push    dword array + 20*4     ; address of array[20]
-        call    print_array
-        add     esp, 8
+  add     rsp, 0x10
 
-        pop     esi
-        pop     ebx
-        mov     eax, 0            ; return back to C
-        leave                     
-        ret
+  mov     rdi, ThirdMsg         ; print out elements 20-29
+  call    _puts
 
-;
-; routine print_array
-; C-callable routine that prints out elements of a double word array as
-; signed integers.
-; C prototype:
-; void print_array( const int * a, int n);
-; Parameters:
-;   a - pointer to array to print out (at ebp+8 on stack)
-;   n - number of integers to print out (at ebp+12 on stack)
+  lea     rdi, [rbx+4*20]
+  mov     rsi, 10
+  call    print_array
+
+  mov     eax, 0            ; return back to C
+  leave
+  ret
+
+;;
+;; routine print_array
+;; C-callable routine that prints out elements of a double word array as
+;; signed integers.
+;; C prototype:
+;; void print_array( const int * a, int n);
+;; Parameters:
+;;   a - pointer to array to print out (at ebp+8 on stack)
+;;   n - number of integers to print out (at ebp+12 on stack)
 
 segment .data
-OutputFormat    db   "%-5d %5d", NEW_LINE, 0
+  OutputFormat    db   "%-5d %5d", NEW_LINE, 0
 
 segment .text
-        global  print_array
+global  print_array
 print_array:
-        enter   0,0
-        push    esi
-        push    ebx
+  enter   0, 0
+  push    rdi
+  push    rsi
 
-        xor     esi, esi                  ; esi = 0
-        mov     ecx, [ebp+12]             ; ecx = n
-        mov     ebx, [ebp+8]              ; ebx = address of array
+  mov     ecx, esi                  ; ecx = n
+  xor     esi, esi                  ; ebx = 0
 print_loop:
-        push    ecx                       ; printf might change ecx!
+  push    rcx                       ; printf might change ecx!
+  push    rsi
+  mov     rax, [rbp-0x08]       ; address of first element
+  mov     rdx, [rax+4*rsi]
+  mov     rsi, [rsp]
+  mov     rdi, OutputFormat
+  call    _printf
 
-        push    dword [ebx + 4*esi]       ; push array[esi]
-        push    esi
-        push    dword OutputFormat
-        call    printf
-        add     esp, 12                   ; remove parameters (leave ecx!)
+  pop     rsi
+  inc     esi
+  pop     rcx
+  loop    print_loop
 
-        inc     esi
-        pop     ecx
-        loop    print_loop
-
-        pop     ebx
-        pop     esi
-        leave
-        ret
-
-
-
-
+  leave
+  ret
