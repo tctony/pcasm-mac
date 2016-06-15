7f7610e436462e5b7210fbe4b4262b5e8b8bc236
Tony Tang <tangchang21@gmail.com>
2016-06-15 16:05:06 +0800

    fix sub6

--- sub6.asm
+++ sub6.asm
@@ -1,52 +1,47 @@
+;;
+;; file: sub6.asm
+;; Subprogram to C interfacing example
 
+;; subroutine calc_sum
+;; finds the sum of the integers 1 through n
+;; Parameters:
+;;   n    - what to sum up to (at [ebp + 8])
+;; Return value:
+;;   value of sum
+;; pseudo C code:
+;; int calc_sum( int n )
+;; {
+;;   int i, sum = 0;
+;;   for( i=1; i <= n; i++ )
+;;     sum += i;
+;;   return sum;
+;; }
+;;
+;; To assemble:
+;; DJGPP:   nasm -f coff sub6.asm
+;; Borland: nasm -f obj  sub6.asm
 
-;
-; file: sub6.asm
-; Subprogram to C interfacing example
-
-; subroutine calc_sum
-; finds the sum of the integers 1 through n
-; Parameters:
-;   n    - what to sum up to (at [ebp + 8])
-; Return value:
-;   value of sum
-; pseudo C code:
-; int calc_sum( int n )
-; {
-;   int i, sum = 0;
-;   for( i=1; i <= n; i++ )
-;     sum += i;
-;   return sum;
-; }
-;
-; To assemble:
-; DJGPP:   nasm -f coff sub6.asm
-; Borland: nasm -f obj  sub6.asm
+default rel
 
 segment .text
-        global  calc_sum
-;
-; local variable:
-;   sum at [ebp-4]
-calc_sum:
-        enter   4,0               ; make room for sum on stack
+global  _calc_sum
+;;
+;; local variable:
+;;   sum at eax
+_calc_sum:
+  enter   0, 0
 
-        mov     dword [ebp-4],0   ; sum = 0
-        mov     ecx, 1            ; ecx is i in pseudocode
+  mov     eax, 0   ; sum = 0
+  mov     ecx, 1            ; ecx is i in pseudocode
 for_loop:
-        cmp     ecx, [ebp+8]      ; cmp i and n
-        jnle    end_for           ; if not i <= n, quit
+  cmp     ecx, edi              ; cmp i and n
+  jnle    end_for           ; if not i <= n, quit
 
-        add     [ebp-4], ecx      ; sum += i
-        inc     ecx
-        jmp     short for_loop
+  add     eax, ecx      ; sum += i
+  inc     ecx
+  jmp     for_loop
 
 end_for:
-        mov     eax, [ebp-4]      ; eax = sum
 
-        leave
-        ret
-
-
-
-
+  leave
+  ret
