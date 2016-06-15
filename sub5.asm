341299614446f1177300d96a7034cd9dc213c9f6
Tony Tang <tangchang21@gmail.com>
2016-06-15 15:21:50 +0800

    fix sub5

--- sub5.asm
+++ sub5.asm
@@ -1,58 +1,58 @@
+;;
+;; file: sub5.asm
+;; Subprogram to C interfacing example
 
-
-;
-; file: sub5.asm
-; Subprogram to C interfacing example
+default rel
 
 %include "asm_io.inc"
 
-; subroutine calc_sum
-; finds the sum of the integers 1 through n
-; Parameters:
-;   n    - what to sum up to (at [ebp + 8])
-;   sump - pointer to int to store sum into (at [ebp + 12])
-; pseudo C code:
-; void calc_sum( int n, int * sump )
-; {
-;   int i, sum = 0;
-;   for( i=1; i <= n; i++ )
-;     sum += i;
-;   *sump = sum;
-; }
-;
-; To assemble:
-; DJGPP:   nasm -f coff sub5.asm
-; Borland: nasm -f obj  sub5.asm
+;; subroutine calc_sum
+;; finds the sum of the integers 1 through n
+;; Parameters:
+;;   n    - what to sum up to (at rdi)
+;;   sump - pointer to int to store sum into (at rsi)
+;; pseudo C code:
+;; void calc_sum( int n, int * sump )
+;; {
+;;   int i, sum = 0;
+;;   for( i=1; i <= n; i++ )
+;;     sum += i;
+;;   *sump = sum;
+;; }
+;;
+;; To assemble:
+;; DJGPP:   nasm -f coff sub5.asm
+;; Borland: nasm -f obj  sub5.asm
 
 segment .text
-        global  calc_sum
-;
-; local variable:
-;   sum at [ebp-4]
-calc_sum:
-        enter   4,0               ; allocate room for sum on stack
-        push    ebx               ; IMPORTANT!
+global  _calc_sum
+;;
+;; local variable:
+;;   sum at [rbp-0x08]
+_calc_sum:
+  enter   0, 0
+  push    rdi
+  push    rsi
+  push    0                     ; sub = 0
+  push    rbx                   ; save rbx IMPORTANT!
 
-        mov     dword [ebp-4],0   ; sum = 0
-        dump_stack 1, 2, 4        ; print out stack from ebp-8 to ebp+16
-        mov     ecx, 1            ; ecx is i in pseudocode
+  dump_stack 1, 1, 3        ; print out stack from ebp-8 to ebp+16
+
+  mov     ecx, 1            ; ecx is i in pseudocode
 for_loop:
-        cmp     ecx, [ebp+8]      ; cmp i and n
-        jnle    end_for           ; if not i <= n, quit
+  cmp     ecx, [rbp - 0x08]      ; cmp i and n
+  jnle    end_for           ; if not i <= n, quit
 
-        add     [ebp-4], ecx      ; sum += i
-        inc     ecx
-        jmp     short for_loop
+  add     [rbp - 0x18], ecx      ; sum += i, add 32 bit int to 64 bit int
+  inc     ecx
+  jmp     for_loop
 
 end_for:
-        mov     ebx, [ebp+12]     ; ebx = sump
-        mov     eax, [ebp-4]      ; eax = sum
-        mov     [ebx], eax
+  mov     rbx, [rbp - 0x10]     ; ebx = sump
+  mov     rax, [rbp - 0x18]      ; eax = sum
+  mov     [rbx], eax             ; rax -> eax
 
-        pop     ebx               ; restore ebx
-        leave
-        ret
-
-
-
-
+  pop     rbx               ; restore rbx
+  sub     rsp, 0x18
+  leave
+  ret
